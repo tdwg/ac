ab587812e53cf9aad28dab81db5e715c8f193690
Niels Klazenga <NielsKlazenga@rbg.vic.gov.au>
2018-08-13 17:41:31 +1000

    Created Jekyll site

---
+++ docs/assets/js/tdwg.ac.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+  $("#theme-sidebar-nav ul").addClass("nav flex-column");
+  $("#theme-sidebar-nav li").addClass("nav-item");
+  $("#theme-sidebar-nav a").addClass("nav-link");
+});
