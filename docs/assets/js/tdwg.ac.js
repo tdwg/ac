f17d81c8720429a52fd476307beb68afda0833a7
Peter Desmet <peter.desmet.work@gmail.com>
2018-10-29 20:52:10 +0100

    Remove all custom theme files and link to remote theme
    https://github.com/tdwg/tdwg-theme-jekyll

--- docs/assets/js/tdwg.ac.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function() {
-  $("#theme-sidebar-nav ul").addClass("nav flex-column");
-  $("#theme-sidebar-nav li").addClass("nav-item");
-  $("#theme-sidebar-nav a").addClass("nav-link");
-});
