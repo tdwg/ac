cec616697b14196346c509f54605e437e0bdba9a
Steve Baskauf <steve.baskauf@vanderbilt.edu>
2023-09-04 20:36:05 -0500

    modify termlist build script to use header template

--- code/build_page.py
+++ code/build_page.py
@@ -9,6 +9,8 @@
 import http_library
 import re
 import pandas as pd
+import yaml
+import requests
 
 # ---------------------------------------------------------------------------
 # retrieve vocabularies members metadata from Github
@@ -277,6 +279,72 @@ def outputMarkdown(text, headerFileName, footerFileName, outFileName):
     header = headerObject.read()
     headerObject.close()
 
+    # Load the contributors YAML file from its GitHub URL
+    contributors_yaml_url = githubBaseUri + config_file_path + contributors_yaml_file
+    contributors_yaml = requests.get(contributors_yaml_url).text
+    if contributors_yaml == '404: Not Found':
+        print('Contributors YAML file not found. Check the URL.')
+        print(contributors_yaml_url)
+        exit()
+    contributors_yaml = yaml.load(contributors_yaml, Loader=yaml.FullLoader)
+
+    # Load the document configuration YAML file from its GitHub URL
+    document_configuration_yaml_url = githubBaseUri + config_file_path + document_configuration_yaml_file
+    document_configuration_yaml = requests.get(document_configuration_yaml_url).text
+    document_configuration_yaml = yaml.load(document_configuration_yaml, Loader=yaml.FullLoader)
+
+    # Build the Markdown for the contributors list
+    contributors = ''
+    for contributor in contributors_yaml:
+        contributors += '[' + contributor['contributor_literal'] + '](' + contributor['contributor_iri'] + ')'
+        if contributor['affiliation'] != '':
+            contributors += ' ([' + contributor['affiliation'] + '](' + contributor['affiliation_uri'] + '))'
+        contributors += ', '
+    contributors = contributors[:-2] # Remove the last comma and space
+
+    # Substitute values of ratification_date and contributors into the header template
+    header = header.replace('{document_title}', document_configuration_yaml['documentTitle'])
+    header = header.replace('{ratification_date}', document_configuration_yaml['doc_modified'])
+    header = header.replace('{created_date}', document_configuration_yaml['doc_created'])
+    header = header.replace('{contributors}', contributors)
+    header = header.replace('{standard_iri}', document_configuration_yaml['dcterms_isPartOf'])
+    header = header.replace('{current_iri}', document_configuration_yaml['current_iri'])
+    header = header.replace('{abstract}', document_configuration_yaml['abstract'])
+    header = header.replace('{creator}', document_configuration_yaml['creator'])
+    header = header.replace('{publisher}', document_configuration_yaml['publisher'])
+    year = document_configuration_yaml['doc_modified'].split('-')[0]
+    header = header.replace('{year}', year)
+
+    # Determine whether there was a previous version of the document.
+    if document_configuration_yaml['doc_created'] != document_configuration_yaml['doc_modified']:
+        # Load versions list from document versions data in the rs.tdwg.org repo and find most recent version.
+        versions_data_url = githubBaseUri + 'docs/docs-versions.csv'
+        versions_list_df = pd.read_csv(versions_data_url, na_filter=False)
+        # Slice all rows for versions of this document.
+        matching_versions = versions_list_df[versions_list_df['current_iri']==document_configuration_yaml['current_iri']]
+        # Sort the matching versions by version IRI in descending order so that the most recent version is first.
+        matching_versions = matching_versions.sort_values(by=['version_iri'], ascending=[False])
+        # The previous version is the second row in the dataframe (row 1).
+        # The version IRI is in the second column (column 1).
+        most_recent_version_iri = matching_versions.iat[1, 1]
+        #print(most_recent_version_iri)
+
+        # Insert the previous version information into the header
+        previous_version_metadata_string = '''Previous version
+    : <''' + most_recent_version_iri + '''>
+
+    '''
+        # Insert the previous version information into the designated slot.
+        header = header.replace('{previous_version_slot}\n\n', previous_version_metadata_string)
+    else:
+        # If there was no previous version, remove the slot from the header.
+        header = header.replace('{previous_version_slot}\n\n', '')
+
+
+
+
+
+
     footerObject = open(footerFileName, 'rt', encoding='utf-8')
     footer = footerObject.read()
     footerObject.close()
@@ -290,10 +358,18 @@ def outputMarkdown(text, headerFileName, footerFileName, outFileName):
 # main routine
 
 # constants
-githubBaseUri = 'https://raw.githubusercontent.com/tdwg/rs.tdwg.org/master/'
+github_branch = 'revise-build-script' # "main" for production, something else for development
+
+# This is the base URL for raw files from the branch of the repo that has been pushed to GitHub
+githubBaseUri = 'https://raw.githubusercontent.com/tdwg/rs.tdwg.org/' + github_branch + '/'
+
 headerFileName = 'termlist-header.md'
 footerFileName = 'termlist-footer.md'
 outFileName = '../docs/termlist/index.md'
+
+config_file_path = 'process/document_metadata_processing/ac_doc_termlist/'
+contributors_yaml_file = 'authors_configuration.yaml'
+document_configuration_yaml_file = 'document_configuration.yaml'
 
 displayOrder = ['http://rs.tdwg.org/dwc/terms/attributes/Management', 
 'http://rs.tdwg.org/dwc/terms/attributes/Attribution', 
